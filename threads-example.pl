ea33e1fe1f6a92022985baa65ea8ffca30841064
morebrackets <morebrackets@users.noreply.github.com>
2020-08-22 17:07:08 -0700

    Create threads-example.pl

---
+++ threads-example.pl
@@ -0,0 +1,74 @@
+use strict;
+use threads(
+	'exit' => 'threads_only', # Prevent whole script from dying if a thread dies
+	# 'stack_size' => 32*4096 # Increase per-thread memory-limit
+);
+use Socket; # for gethostbyname
+$|++; # flush output
+
+my $numThreads = 24; # 1x-5x number of cores on *nix. Less on windows.
+my $numLookupsPerThread = 20;
+my $hostlength = 3;
+my @letters = split(//,'qwertyuiopasdfghjklzxcvbnm');
+my $lLength = $#letters;
+
+my $beginMain = time;
+my $totalLookups = $numThreads*$numLookupsPerThread;
+
+print "*** Main begin: $numThreads threads X $numLookupsPerThread lookups = $totalLookups total lookups\n";
+
+# Create $numThreads threads
+for my $n (1..$numThreads) {
+
+	my $t = threads->create(sub { findip($n) });
+
+	if (!$t) { 
+		print "\nSystem says ".int(threads->list(threads::running))." is too many threads. Retrying...\n\n";
+		sleep 3;
+		redo;
+	}
+}
+
+# Wait for threads to complete
+while (int(threads->list(threads::running))) {
+	sleep 1;
+}
+
+my $elapsed = time - $beginMain;
+print "*** Main complete\n$totalLookups in $elapsed sec (".int($totalLookups/$elapsed)." per sec)\n";
+
+
+# Threaded function
+sub findip {
+	my $id = shift;
+	local $SIG{KILL} = sub { threads->exit; }; #### IMPORTANT
+	print "* t$id: begin\n";
+
+	for my $n (1..$numLookupsPerThread){
+		my $dom = &randStr($hostlength) . '.com';
+		my $ip = 'na';
+
+		# lookup
+		my $begin = time;
+		my $packed_ip = gethostbyname($dom);
+
+	    $ip = inet_ntoa($packed_ip) if (defined $packed_ip);
+
+	    print "t$id\: $dom = $ip (".(time - $begin)." sec)\n";
+	}
+
+	print "t$id: complete\n";
+
+	# Detach (signal to controller that thread work has been completed)
+	threads->detach();
+}
+
+# Random string generator
+sub randStr {
+	my $length = shift;
+	my $out = '';
+	for(1..$length){
+		$out .= $letters[int(rand($lLength))];
+	}
+	return $out;
+}
